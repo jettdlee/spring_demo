0c5a4c3b64ae69ab8899db707a52a6dacf6652fb
Jet Lee <jettdlee@gmail.com>
2021-12-29 14:12:03 +0000

    Create put endpoint

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,26 @@ public class StudentService {
       throw new IllegalStateException("Student with id " + studentId + " does not exist");
     }
     studentRepository.deleteById(studentId);
+  }
+
+  @Transactional
+  public void updateStudent(Long studentId, String name, String email) {
+    Student student = studentRepository.findById(studentId).orElseThrow(
+        () -> new IllegalStateException("Student with id " + studentId + " does not exist")
+    );
+
+    if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+      student.setName(name);
+    }
+
+    if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+      Optional<Student> studentOptional = studentRepository
+          .findStudentByEmail(student.getEmail());
+
+      if (studentOptional.isPresent()) {
+        throw new IllegalStateException("Email Taken");
+      }
+      student.setEmail(email);
+    }
   }
 }
